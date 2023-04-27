25fc908d87f9bdbc56f4366825218a8d05faa811
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-27 15:45:14 +0700

    Add files via upload

---
+++ GasStations.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class GasStation1 {
+
+    public GasStation1(boolean[] gas, int[] capa) {
+
+    }
+}
+
+public class GasStations {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+    }
+}
