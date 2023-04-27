2ab598f9feba03567f432c0a9892ac86ba3cff78
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-27 15:08:22 +0700

    Add files via upload

---
+++ CookieFactory.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+class CookiePlanner {
+
+    int F;
+    int B;
+    int flour = 0;
+    int butter = 0;
+    int bags = 0;
+
+    public CookiePlanner(int F, int B) {
+        this.F = F;
+        this.B = B;
+    }
+
+    int calculate() {
+
+    }
+
+    boolean produce(int amount) {
+
+    }
+}
+
+public class CookieFactory {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int F = scan.nextInt();
+        int B = scan.nextInt();
+        CookiePlanner planner = new CookiePlanner(F, B);
+
+        int n = scan.nextInt();
+        for (int i = 0; i < n; ++i) {
+            int k = scan.nextInt();
+            int a = scan.nextInt();
+             ?  ?  ?
+        }
+         ?  ?  ?;
+    }
+}
